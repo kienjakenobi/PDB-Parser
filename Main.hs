9ce90a1193b58bd0f02c5f5513698bd261a41b63
Alex Dunn <utterrot@gmail.com>
2012-02-26 13:15:21 -0600

    Separated out the parsing functions into PDBParsing.hs.  This file is currently useless, just used as a staging ground for what you want the program to perform.

--- Main.hs
+++ Main.hs
@@ -1,200 +1,178 @@
--- Author: Alex Dunn
--- Last Modified: 25 January 2012
--- Description: Data structures and functions to import and store
---  information from a Protein Database file
-
--- Usage:
---  Open a PDB file: p <- 'pr [file]'
-
--- Top Level Functions (Those functions not referenced by any other function)
---  pr (I=PDB file location as a string, O=List of Atoms)
+-- Main.hs
+-- main function to execute for PDB Parser
 
 module Main where
 
-import qualified Data.ByteString as B -- ByteString class for efficient string operations
-import qualified Data.ByteString.Char8 as BC -- unpack, readInt
-import Data.ByteString.Lex.Double -- readDouble, from the bytestring-lexing module
-import Data.Maybe -- fromJust
-import Data.List -- \\
-
 -- Custom libraries:
-import DataStructures
-import PRLength
-import PRCaDist
+import PDBDataStructures
+--import PRLength
+--import PRCaDist
 import Meta
-import CustomMath
--- import System.Environment --getArgs
+import PRAromatics
+import PDBParsing
 
---Read a ByteString into an Int
-ri :: B.ByteString -> Int
-ri bs = fst $ fromJust $ BC.readInt bs
+-- TODO: Remove all instances of head and fromJust via pattern matching
 
---Read a ByteString into a Double
-rd :: B.ByteString -> Double
-rd bs = fst $ fromJust $ readDouble bs
+pheMain :: FilePath -> FilePath -> IO ()
+pheMain dest s = do
+    -- Status report
+    print $ "Working on " ++ s
+    -- Get the file
+    p <- pr s
+    -- Get only the A altlocs
+    let theas = getAlt (head $ proModels p) ['A', ' ']
+    -- Sort the entire list of proteins into residues
+    let resAs = groupRes theas
 
--- ByteString -> [Word8]
--- Data.ByteString.unpack ByteString = B.unpack String
-
--- Word8 -> Char
--- Data.ByteString.Internal.w2c Word8
-
--- Char -> Word8
--- Data.ByteString.Internal.c2w Char
-
--- String -> ByteString
---Data.ByteString.Char8.pack String = BC.pack String
-
--- ByteString -> String
--- Data.ByteString.Char8.unpack string = BC.unpack String
-
--- Take substring of a larger ByteString
--- Take start index a and end index b, indices starting at 1
--- Also remove spaces
-get :: Int -> Int -> B.ByteString -> B.ByteString
-get a b str = B.filter (/=32) $ B.drop (a - 1) $ B.take b str
-
--- atomize
--- This is the actual parser
--- Take a single line of 
-at :: B.ByteString -> Maybe Atom
-at str
-    -- Test that it is an ATOM entry
-    | recName == B.pack a8 = Just Atom 
-        {atomRef = serial
-        , atomName = name
-        , atomAlt = altloc
-        , atomAmino = amino
-        , atomChain = chain
-        , atomRes = resNum
-        , atomPos = Point3D x y z
-        , atomSymbol = symbol} 
-    | otherwise = Nothing where
-    -- Get the title of the record
-    recName = get 1 6 str
-    --Attributes of the atom:
-    serial = ri $ get 7 11 str
-    name = get 13 16 str
-    altloc = B.index str 16
-    amino = get 18 20 str
-    chain = B.index str 21
-    resNum = ri $ get 23 26 str
-    x = rd $ get 31 38 str
-    y = rd $ get 39 46 str
-    z = rd $ get 47 54 str
-    symbol = get 77 78 str
-    -- "ATOM" as a [Word8]
-    a8 = [65,84,79,77]
-
--- Get all the helices out of a PDB file
-ht :: B.ByteString -> Maybe Helix
-ht str
-    -- Test that it is a HELIX entry
-    | recName == B.pack h8 = Just Helix 
-        {helixResList = (startLoc, endLoc)} 
-    | otherwise = Nothing where
-    -- Get the title of the record
-    recName = get 1 6 str
-    --Attributes of the sheet:
-    startLoc = ri $ get 22 25 str
-    endLoc = ri $ get 34 37 str
-    -- "HELIX" as a [Word8]
-    h8 = [72,69,76,73,88]
-
--- Get all the strips of sheets out of a PDB file
-st :: B.ByteString -> Maybe Sheet
-st str
-    -- Check if it is a SHEET entry
-    | recName == B.pack s8 = Just Sheet
-        {sheetResList = (startLoc, endLoc)}
-    | otherwise = Nothing where
-    -- Get the title of the record
-    recName = get 1 6 str
-    --Attributes of the sheet:
-    startLoc = ri $ get 23 26 str
-    endLoc = ri $ get 34 37 str
-    -- "SHEET" as a [Word8]
-    s8 = [83,72,69,69,84]
-
--- proteinize
--- Perform the listize, furtherListize, and atoms functions on a file
-pr :: FilePath -> IO Protein
-pr s = do
-    pdb <- B.readFile s
-    -- Split the file on line breaks
-    let split = B.splitWith (==10) pdb
-    return $ mapMaybe at split
-
--- Get all the helices which are in the PDB
-hr :: FilePath -> IO [Helix]
-hr s = do
-    -- Open the file
-    pdb <- B.readFile s
-    -- Split the file on line breaks
-    let split = B.splitWith (==10) pdb
-    return $ mapMaybe ht split
+    -- Get the Phe residues
+    let phe = getAmino theas "PHE"
+    -- Group the Phe atoms by residue
+    let resPhe = groupRes phe
+    -- Get a list of all the corresponding aromatics
+    let aromatics = getAromatics resPhe
+    -- Get all HBond situations
+    let hbonds = map (hBondMFinder resAs) aromatics
     
+    -- Only save those which have content
+    case (concat hbonds) of
+        [] -> print $ "None found in " ++ s
+        _ -> appendFile dest (show $ concat hbonds)
+
+pheSide :: FilePath -> FilePath -> IO ()
+pheSide dest s = do
+    -- Status report
+    print $ "Working on " ++ s
+    -- Get the file
+    p <- pr s
+    -- Get only the A altlocs
+    let theas = getAlt (head $ proModels p) ['A', ' ']
+    -- Sort the entire list of proteins into residues
+    let resAs = groupRes theas
+
+    -- Get the Phe residues
+    let phe = getAmino theas "PHE"
+    -- Group the Phe atoms by residue
+    let resPhe = groupRes phe
+    -- Get a list of all the corresponding aromatics
+    let aromatics = getAromatics resPhe
+    -- Get all HBond situations
+    let hbonds = map (hBondSFinder resAs) aromatics
     
--- Get all sheet strips which are in the PDB
-sr :: FilePath -> IO [Sheet]
-sr s = do
-    -- Open the file
-    pdb <- B.readFile s
-    -- Split the file on line breaks
-    let split = B.splitWith (==10) pdb
-    return $ mapMaybe st split
+    -- Only save those which have content
+    case (concat hbonds) of
+        [] -> print $ "None found in " ++ s
+        _ -> appendFile dest (show $ concat hbonds)
+    
+tyrMain :: FilePath -> FilePath -> IO ()
+tyrMain dest s = do
+    -- Status report
+    print $ "Working on " ++ s
+    -- Get the file
+    p <- pr s
+    -- Get only the A altlocs
+    let theas = getAlt (head $ proModels p) ['A', ' ']
+    -- Sort the entire list of proteins into residues
+    let resAs = groupRes theas
 
--- Get all atoms which are inside a secondary structure sheet
-getSheets :: Protein -> [[Atom]]
-getSheets = undefined
+    -- Get the Phe residues
+    let tyr = getAmino theas "TYR"
+    -- Group the Phe atoms by residue
+    let resTyr = groupRes tyr
+    -- Get a list of all the corresponding aromatics
+    let aromatics = getAromatics resTyr
+    -- Get all HBond situations
+    let hbonds = map (hBondMFinder resAs) aromatics
+    
+    -- Only save those which have content
+    case (concat hbonds) of
+        [] -> print $ "None found in " ++ s
+        _ -> appendFile dest (show $ concat hbonds)
 
--- Get all atoms which are inside a secondary structure loop
-getLoops :: Protein -> [[Atom]]
-getLoops = undefined
+tyrSide :: FilePath -> FilePath -> IO ()
+tyrSide dest s = do
+    -- Status report
+    print $ "Working on " ++ s
+    -- Get the file
+    p <- pr s
+    -- Get only the A altlocs
+    let theas = getAlt (head $ proModels p) ['A', ' ']
+    -- Sort the entire list of proteins into residues
+    let resAs = groupRes theas
+
+    -- Get the Phe residues
+    let tyr = getAmino theas "TYR"
+    -- Group the Phe atoms by residue
+    let resTyr = groupRes tyr
+    -- Get a list of all the corresponding aromatics
+    let aromatics = getAromatics resTyr
+    -- Get all HBond situations
+    let hbonds = map (hBondSFinder resAs) aromatics
+    
+    -- Only save those which have content
+    case (concat hbonds) of
+        [] -> print $ "None found in " ++ s
+        _ -> appendFile dest (show $ concat hbonds)
+
+trpMain :: FilePath -> FilePath -> IO ()
+trpMain dest s = do
+    -- Status report
+    print $ "Working on " ++ s
+    -- Get the file
+    p <- pr s
+    -- Get only the A altlocs
+    let theas = getAlt (head $ proModels p) ['A', ' ']
+    -- Sort the entire list of proteins into residues
+    let resAs = groupRes theas
+
+    -- Get the Phe residues
+    let trp = getAmino theas "TRP"
+    -- Group the Phe atoms by residue
+    let resTrp = groupRes trp
+    -- Get a list of all the corresponding aromatics
+    let aromatics = getAromatics resTrp
+    -- Get all HBond situations
+    let hbonds = map (hBondMFinder resAs) aromatics
+    
+    -- Only save those which have content
+    case (concat hbonds) of
+        [] -> print $ "None found in " ++ s
+        _ -> appendFile dest (show $ concat hbonds)
+
+trpSide :: FilePath -> FilePath -> IO ()
+trpSide dest s = do
+    -- Status report
+    print $ "Working on " ++ s
+    -- Get the file
+    p <- pr s
+    -- Get only the A altlocs
+    let theas = getAlt (head $ proModels p) ['A', ' ']
+    let resAs = groupRes theas
+
+    -- Get the TRP residues
+    let trp = getAmino theas "TRP"
+    -- Group the TRP atoms by residue
+    let resTrp = groupRes trp
+    -- Get a list of all the corresponding aromatics
+    let aromatics = getAromatics resTrp
+    -- Get all HBond situations
+    let hbonds = map (hBondSFinder resAs) aromatics
+    
+    -- Only save those which have content
+    case (concat hbonds) of
+        [] -> print $ "None found in " ++ s
+        _ -> appendFile dest (show $ concat hbonds)
 
 -- TODO: Define this such that it takes a PDB file as its argument and
---  flags to perform operations on the PDB file
+--  flags to perform operations on the PDB file, then the program will
+--  be ready to be used in compiled form
 main :: IO ()
 main = do
-    p <- pr "/Users/mortal/Desktop/3O4P.pdb"
-    -- Get generic data about protein
-    -- Get rid of the B altlocs
-    let theas = getAlt p ['A', ' ']
-    --print theas
-    --print $ length theas
-    
-    -- Get the sheet and helix data structures
-    helices <- hr "/Users/mortal/Desktop/3O4P.pdb"
-    sheets <- sr "/Users/mortal/Desktop/3O4P.pdb"
-    --print $ length helices
-    --print $ length sheets
-    --print helices
-    --print sheets
-    
-    let helixAtomsList = map (getHelixAtoms theas) helices
-    let sheetAtomsList = map (getSheetAtoms theas) sheets
-    --print helixAtomsList
-    --print sheetAtomsList
-    let helixAtoms = concat helixAtomsList
-    let sheetAtoms = concat sheetAtomsList
-    let loopAtoms = getLoopAtoms theas helices sheets
---    print $ length helixAtoms
---    print $ length sheetAtoms
---    print $ length loopAtoms
-    --let resHelix = groupRes helixAtoms
-    --let resSheet = groupRes sheetAtoms
-    --let resLoop = groupRes loopAtoms
-    --print $ length resHelix
-    --print $ length resSheet
-    --print $ length resLoop
-
-    let hcas = getAtomName helixAtoms "CA"
-    let scas = getAtomName sheetAtoms "CA"
-    let lcas = getAtomName loopAtoms "CA"
-    --let hdist = concat $ map (lindist hcas) [1,2,3,4]
-    --print hdist
-    --let sdist = concat $ map (lindist scas) [1..152]
-    --print sdist
-    let ldist = concat $ map (lindist lcas) [1..155]
-    print ldist
-    
+    -- Get a list of all PDB files to iterate over
+    pdbs <- folderFiles "/Users/mortal/Downloads/nrpdb/"
+    -- Set the location to save data
+    let dest = "/Users/mortal/Desktop/pheMain_R=ToCenter.txt"
+    -- Make sure the destination file is empty
+    writeFile dest ""
+    -- Map the function across all the PDB files, throwing out the result,
+    --  which is printed
+    mapM_ (pheMain dest) pdbs
